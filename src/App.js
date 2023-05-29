cfb55405a69d1f4df432fc23b28ff1c3f3a5cbfe
stevebiczyk <stevebiczyk@gmail.com>
2023-05-29 11:12:03 +0000

    The useState() Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import HooksCounter from "./components/HooksCounter";
-import ClassCounter from "./components/ClassCounter";
+import ControlledFormHooks from "./components/ControlledFormHooks";
 
 function App() {
   return <div className="App">
-    <HooksCounter />
-    <ClassCounter />
+    <ControlledFormHooks />
   </div>;
 }
 
